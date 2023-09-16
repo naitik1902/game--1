900fe760114bac9a2671ba8c16244998d0a0406e
naitik1902 <139570941+naitik1902@users.noreply.github.com>
2023-09-16 19:35:47 +0530

    Add files via upload

---
+++ game 1/sketch.js
@@ -0,0 +1,158 @@
+var balloon,balloon_fly;
+var scene;
+
+var cloud,bird ,bird_fly, invisibleline,invisibleline2
+
+var cloudsgroup , birdgroup
+var score = 0;
+var Highscore = 0;
+var PLAY = 1 
+var END = 0 
+var gameState = PLAY
+
+var gameOver, restart;
+
+function preload(){
+
+balloon_fly = loadImage("balloon1.png");
+  
+cloudImage = loadImage("cloud.png");
+
+gameOverImage = loadImage("gameOver.png")
+restartImage = loadImage("restart.png")
+
+} 
+
+function setup(){
+  
+  createCanvas(800,400);
+  
+  balloon = createSprite(50,300,20,20)
+  // balloon.addImage(balloon_fly);
+  // balloon.scale = 0.5;
+
+ 
+  invisibleline = createSprite(400,390,800,10);
+  invisibleline2 = createSprite(400,10,800,10);
+
+  gameOver = createSprite(400,200,10,10);
+  gameOver.addImage(gameOverImage);
+
+  restart = createSprite(400,250,10,10);
+  restart.addImage(restartImage);
+  restart.scale = 0.8
+  
+  cloudsgroup = createGroup();
+  birdgroup = createGroup();
+}
+
+function draw(){
+  background("skyblue")
+  
+  text("Score:" + score,700,50)
+    text("Highscore:" + Highscore,700,70)
+    score = score + Math.round(frameCount/150);
+    
+  if(gameState === PLAY){
+   
+    invisibleline.visible = false
+    invisibleline2.visible = false
+
+   
+    gameOver.visible = false;
+    restart.visible = false;
+
+    
+   
+    if(keyDown("space")){
+      balloon.velocityY = -10
+    }
+    balloon.velocityY+= 0.5
+    balloon.collide(invisibleline);
+    balloon.collide(invisibleline2);
+  
+    
+
+    if(score > Highscore){
+      Highscore = score
+    }
+
+    spawnClouds()
+    spawnbirds()
+
+    if(balloon.isTouching(cloudsgroup)){
+        gameState = END;
+     }
+
+     if(balloon.isTouching(birdgroup)){
+      gameState = END;
+   }
+
+    }
+  else if (gameState === END){
+    gameOver.visible = true;
+    restart.visible = true;
+
+      balloon.velocityY = 0;
+      
+      cloudsgroup.setVelocityXEach(0);
+      birdgroup.setVelocityXEach(0);
+      cloudsgroup.setLifetimeEach(-1);
+      birdgroup.setLifetimeEach(-1);
+      
+      invisibleline.visible = false
+    invisibleline2.visible = false
+      frameCount = 0 
+  }
+
+  if(mousePressedOver(restart)){
+    reset();
+  }
+  
+
+  
+ 
+  
+ 
+
+  drawSprites();
+}
+
+function reset(){
+  gameState = PLAY;
+  score = 0;
+  gameOver.visible = false;
+  restart.visible = false;
+  birdgroup.destroyEach();
+  cloudsgroup.destroyEach();
+  
+}
+
+function spawnClouds (){
+  if(frameCount % 60 === 0){
+  var cloud = createSprite(800,200,10,20);
+  cloud.y = Math.round(random(400,50));
+  cloud.addImage(cloudImage);
+  cloud.scale= 0.5;
+  cloud.velocityX=-4;
+  cloud.lifeTime=250;
+ cloudsgroup.add(cloud)
+  }
+}
+
+function spawnbirds (){
+  if(frameCount % 60 === 0){
+    var bird = createSprite(790,200,10,20);
+    bird.y = Math.round(random(400,50));
+    // bird.addAnimation(bird_fly);
+    // bird.scale= 0.5;
+    bird.velocityX=-4;
+    bird.lifeTime=250;
+    birdgroup.add(bird)
+    }
+
+  }
+
+
+
+
